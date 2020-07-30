b0f15fe9eeba70136820f5a78900d704baf60100
Asritha Bodepudi <22bodepudi2@lexingtonma.org>
2020-07-30 13:08:45 -0400

    Initial Commit

---
+++ MoleScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoleScan
+//
+//  Created by 65,115,114,105,116,104,98 on 7/30/20.
+//  Copyright © 2020 Asritha Bodepudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
